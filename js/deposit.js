827690e1b0e00b2c248563133bc428fda5d3e919
Mushfiq80 <aurpon80@gmail.com>
2023-02-15 04:03:59 +0600

    deposit done

--- js/deposit.js
+++ js/deposit.js
@@ -2,6 +2,13 @@
 document.getElementById('btn-deposit').addEventListener('click',function(){
     // step-2 get the deposit amount 
     const DepositField = document.getElementById('deposit-amount');
-    const depositAmount = DepositField.value;
+    const newDepositAmount = DepositField.value;
+    
+    // step-3 Now catch the input filed entry and pass it to the deposit box 
+    const depositTotalElement = document.getElementById('deposit-total');
+    const previousDepositTotal = depositTotalElement.innerText;
+
+    const currentDeposit = parseFloat(previousDepositTotal) + parseFloat(newDepositAmount);
+    depositTotalElement.innerText = currentDeposit;
 
 })
